a9472de9a5826ccd60dd47101ba664d54eac0a62
Alper <alperemreeren@hotmail.com>
2022-05-09 15:54:13 +0300

    Initial boilerplate

--- src/routes/fetchAllUsers.ts
+++ src/routes/fetchAllUsers.ts
@@ -4,15 +4,12 @@ export default function fetchAllUsers(request: Request, response: Response) {
     const users = [
         {
             name: 'Stefano',
-            age: ~25-30
         },
         {
             name: 'Alper',
-            age: 17
         },
         {
             name: 'Joe',
-            age: 20
         }
     ];
 
