11a2065ad7b3a4e89e2dbbd420d0d4bfa6106cc1
Alper <alperemreeren@hotmail.com>
2022-05-09 15:55:05 +0300

    Added Database Connection

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,28 @@
 import express, { Application, Request, Response } from "express";
 import http from 'http';
+import mongoose from 'mongoose';
 import 'dotenv/config';
 
+// Import Routes
+import fetchAllUsers from "./routes/fetchAllUsers";
+
+// Express app
 const app: Application = express();
 const server = new http.Server(app);
 
-server.listen(process.env.PORT, () => console.log(`Running on Port: ${process.env.PORT}`));
+// Connect to MongoDB
+const dbURI: any = process.env.DBURI;
+mongoose.connect(dbURI)
+    .then((result) => {
+        console.log('Successfully Connected to Database');
+        
+        // Start the server after the connection is established with database
+        startServer();
+    })
+    .catch((err) => console.log(err));
+
+// Endpoints
+app.get('/fetchAllUsers', fetchAllUsers);
+
+// Initialize Server
+const startServer = () => server.listen(process.env.PORT, () => console.log(`Running on Port: ${process.env.PORT}`));
